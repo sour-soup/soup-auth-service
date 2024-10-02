53330b36e894146a35acd437dda035451b916e35
sour-soup <kiziyepi8@gmail.com>
2024-10-02 20:53:36 +0400

    feat(repository): add UserEntity and UserRepository

---
+++ src/main/java/org/soup/authservice/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package org.soup.authservice.repository;
+
+import org.soup.authservice.repository.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, UUID> {
+    Optional<UserEntity> findByUsername(String username);
+}
---
+++ src/main/java/org/soup/authservice/repository/entity/UserEntity.java
@@ -0,0 +1,25 @@
+package org.soup.authservice.repository.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "users")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+}
